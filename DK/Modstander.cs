98496c9a6d1694f3e0afe3573ac37e714bb495e2
NikolajNK <niko_kristensen@yahoo.dk>
2022-12-18 14:29:31 +0100

    Implementeret Modstander Klasse

--- DK/Modstander.cs
+++ DK/Modstander.cs
@@ -1,8 +1,45 @@
-﻿namespace MatchMakerDBU.DK
-{
-    public class Modstander
-    {
+﻿using MatchMakerDBU.Model;
 
-        //Modstander. Lav det samme som i DK, men uden spillerne
+namespace MatchMakerDBU.DK
+{
+    public class Modstander : Holdspiller
+    {
+        private List<Spiller> _spillere = new List<Spiller>()
+        {
+
+        };
+
+        public List<Spiller> GetAllSpillere()
+        {
+            return new List<Spiller>(_spillere);
+        }
+
+        public void DeleteSpiller(int nummer)
+        {
+            Spiller sletSpiller = FindSpiller(nummer);
+            _spillere.Remove(sletSpiller);
+        }
+
+        public Spiller FindSpiller(int nummer)
+        {
+            foreach (Spiller s in _spillere)
+            { 
+                if (s.Nummer == nummer)
+                {
+                    return s;
+                }
+            }
+
+            throw new KeyNotFoundException();
+        }
+
+        public void EditSpiller(Spiller newValues)
+        {
+            Spiller editSpiller = FindSpiller(newValues.Nummer);
+    
+            editSpiller.Name = newValues.Name;
+            editSpiller.Rating = newValues.Rating;
+            editSpiller.Type = newValues.Type;
+        }
     }
 }
