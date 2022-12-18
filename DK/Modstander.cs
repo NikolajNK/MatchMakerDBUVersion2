a061f4bdd137fdbfdad99bb3ebcab824c4a08618
NikolajNK <niko_kristensen@yahoo.dk>
2022-12-19 00:11:46 +0100

    Tilføjet Metode: MinMax rating - MAX 1 unikt nummer pr. hold

--- DK/Modstander.cs
+++ DK/Modstander.cs
@@ -1,46 +1,46 @@
-﻿using MatchMakerDBU.Model;
+﻿//using MatchMakerDBU.Model;
 
-namespace MatchMakerDBU.DK
-{
-    public class Modstander : Holdspiller
-    {
-        private List<Spiller> _spillere = new List<Spiller>()
-        {
+//namespace MatchMakerDBU.DK
+//{
+//    public class Modstander : Holdspiller
+//    {
+//        private List<Spiller> _spillere = new List<Spiller>()
+//        {
 
-        };
+//        };
 
-        public List<Spiller> GetAllSpillere()
-        {
-            return new List<Spiller>(_spillere);
-        }
+//        public List<Spiller> GetAllSpillere()
+//        {
+//            return new List<Spiller>(_spillere);
+//        }
 
-        public void DeleteSpiller(int nummer)
-        {
-            Spiller sletSpiller = FindSpiller(nummer);
-            _spillere.Remove(sletSpiller);
-        }
+//        public void DeleteSpiller(int nummer)
+//        {
+//            Spiller sletSpiller = FindSpiller(nummer);
+//            _spillere.Remove(sletSpiller);
+//        }
 
-        public Spiller FindSpiller(int nummer)
-        {
-            foreach (Spiller s in _spillere)
-            {
-                if (s.Nummer == nummer)
-                {
-                    return s;
-                }
-            }
+//        public Spiller FindSpiller(int nummer)
+//        {
+//            foreach (Spiller s in _spillere)
+//            {
+//                if (s.Nummer == nummer)
+//                {
+//                    return s;
+//                }
+//            }
 
-            throw new KeyNotFoundException();
-        }
+//            throw new KeyNotFoundException();
+//        }
 
-        public void EditSpiller(Spiller newValues)
-        {
-            Spiller editSpiller = FindSpiller(newValues.Nummer);
+//        public void EditSpiller(Spiller newValues)
+//        {
+//            Spiller editSpiller = FindSpiller(newValues.Nummer);
 
-            editSpiller.Name = newValues.Name;
-            editSpiller.Rating = newValues.Rating;
-            editSpiller.Type = newValues.Type;
+//            editSpiller.Name = newValues.Name;
+//            editSpiller.Rating = newValues.Rating;
+//            editSpiller.Type = newValues.Type;
 
-        }
-    }
-}
+//        }
+//    }
+//}
